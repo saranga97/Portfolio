ad074bd3ca68a0292e95df9c7afc42f55f32ffa6
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-07-25 15:45:10 +0530

    API key

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_6mfqywut_3oyC8m53XbJKuEYosSMxTp7o');
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form from Portfolio <onboarding@resend.dev>",
-      to: "masechkacat@gmail.com",
+      to: "saranga.karunasundara@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
